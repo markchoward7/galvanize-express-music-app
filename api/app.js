31b2522bb396f57e4c8a8fe5e0755c77820db424
markchoward7 <markchoward7@outlook.com>
2020-06-23 15:47:19 -0500

    fixed fetching

--- api/app.js
+++ api/app.js
@@ -13,8 +13,8 @@ var fetch = require('node-fetch')
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(cors());
 app.use(logger('dev'));
@@ -27,60 +27,67 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/users', usersRouter);
 // app.use("/testAPI", testAPIRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 var musicData = []
 fetch("https://itunes.apple.com/search?term=the+beatles").then(response => response.json()).then(data => {
-  musicData.push({
+  newArtist = {
     artistName: data.results[0].artistName,
     artistId: data.results[0].artistId,
     albums: []
-  })
+  }
+  musicData.push(newArtist)
+  LoadAlbums(newArtist)
 })
 fetch("https://itunes.apple.com/search?term=johnny+cash").then(response => response.json()).then(data => {
-  musicData.push({
+  newArtist = {
     artistName: data.results[0].artistName,
     artistId: data.results[0].artistId,
     albums: []
-  })
+  }
+  musicData.push(newArtist)
+  LoadAlbums(newArtist)
 })
-for (const artist of musicData) {
-  fetch(`https://itunes.apple.com/lookup?id=${artist.artistId}&entity=album`).then(response => response.json().then(data => {
+function LoadAlbums(artist) {
+  fetch(`https://itunes.apple.com/lookup?id=${artist.artistId}&entity=album`).then(response => response.json()).then(data => {
     for (const album of data.results.slice(1)) {
-      artist.albums.push({
+      newAlbum = {
         albumName: album.collectionName,
         albumId: album.collectionId,
         artworkUrl: album.artworkUrl100,
         songs: []
-      })
-    }
-  }))
-  for (const album of artist.albums) {
-    fetch(`https://itunes.apple.com/search?attribute=albumTerm&term=${album.collectionName.replace(' ', '+')}&entity=song`).then(response => response.json()).then(data => {
-      for (const song of data.results) {
-        album.songs.push({
-          songName: song.trackName,
-          songId: song.trackId,
-          previewUrl: song.previewUrl
-        })
+      }
+      artist.albums.push(newAlbum)
+      LoadSongs(newAlbum)
       }
     })
   }
+function LoadSongs(album) {
+  fetch(`https://itunes.apple.com/search?attribute=albumTerm&term=${album.albumName.replace(' ', '+')}&entity=song`).then(response => response.json()).then(data => {
+    for (const song of data.results) {
+      album.songs.push({
+        songName: song.trackName,
+        songId: song.trackId,
+        previewUrl: song.previewUrl
+      })
+    }
+  })
 }
+    
 
 app.get('/', (req, res) => {
   returnData = []
