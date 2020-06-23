b727034a55a57f0625f5f176da94c266c43f3d6c
noxsios <harryrndzz@gmail.com>
2020-06-23 22:17:59 +0200

    Updated api app.js

--- api/app.js
+++ api/app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
-
-var data = require("./data.json")
+var fetch = require("node-fetch")
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
