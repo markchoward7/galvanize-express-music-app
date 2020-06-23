c8d70b31bb96d9f1fd3c52720cc4eb05c047a9be
noxsios <harryrndzz@gmail.com>
2020-06-23 20:35:20 +0200

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [apiResponse, setapiResponse] = useState("");
+
+  let callAPI = () => {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((res) => setapiResponse(res));
+  };
+
+    useEffect(() => {
+      callAPI();
+    }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="App-intro">;{apiResponse}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
