866c516357b03089c1536ef0ee11b6807348ae1e
noxsios <harryrndzz@gmail.com>
2020-06-23 20:58:24 +0200

    Added Material theme + overall structure setup

---
+++ client/src/components/Title.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const Title = () => {
+  return (
+    <div style={{ paddingTop: "1rem" }}>
+      <Typography variant="caption">
+        Product proudly brought to you by Harry Randazzo ( Razzle-Dazzle )
+        <br></br>
+        Reducing user 💢 and increasing user 💯 like the ✨ I am
+      </Typography>
+    </div>
+  );
+};
+
+export default Title;
